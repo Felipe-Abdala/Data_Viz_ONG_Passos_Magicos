d05b8db6402fee71fe8581245e088ba8fa501bec
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2025-02-14 20:53:14 -0300

    Update 4_🌎_Conclusão.py

--- pages/4_🌎_Conclusão.py
+++ pages/4_🌎_Conclusão.py
@@ -25,15 +25,15 @@ with coluna1:
     st.success('''1. __Aumento no conjunto do desempenho acadêmico, psicossocial e psicopedagógico (INDE):__
     - Entre os anos de 2022 e 2024, o resultado global do __*INDE*__, envolvendo as __áreas acadêmicas, psicossociais e psicopedagógicas__ totalizaram melhoras nos alunos do programa da ONG Passos Mágicos, tanto em :blue[__escolas públicas evoluindo de 7,2 para 7,3__], quanto para :blue[__particulares migrando de 7,7 para 7,9__].
              ''')
-    st.success('''1. __Aumento no conjunto do desempenho acadêmico, psicossocial e psicopedagógico (INDE):__
-    - Entre os anos de 2022 e 2024, o resultado global do __*INDE*__, envolvendo as __áreas acadêmicas, psicossociais e psicopedagógicas__ totalizaram melhoras nos alunos do programa da ONG Passos Mágicos, tanto em :blue[__escolas públicas evoluindo de 7,2 para 7,3__], quanto para :blue[__particulares migrando de 7,7 para 7,9__].
+    st.success('''2. __Aumento no conjunto do desempenho acadêmico, psicossocial e psicopedagógico (INDE):__
+    - O programa tem atendido efetivamente uma demanda intrínseca aos seus valores, o de ter impacto social, conforme visto na proporção de :blue[__alunos de escola pública__] versus __de escola particulares__, sendo o primeiro grupo o mais representativo.
              ''')
 with coluna2:
     st.warning('######  A partir da avaliação dos pontos a melhorar do programa, conclui-se que:', icon="⚠️")
-    st.write('''1. __Dificuldade no desempenho acadêmico:__
+    st.warning('''1. __Dificuldade no desempenho acadêmico:__
     - Durante os anos avaliados, identificou-se que o desempenho escolar (representado pelo "Indicador de Desempenho escolar", sigla IDA) apresentou queda quando comparados os anos de 2022 com o de 2024, tanto na média das :red[__escolas públicas, indo de 6,45 para 6,22__], quanto em :red[__escolas particulares, indo de 7,1 para 7,0__].
              ''')
-    st.write('''2. __Evasão diminuindo, porém ainda sob atenção:__
-    - Em 2024, o nível de adesão alcançou em :blue[__1.054 matrículas ativas__], contra :red[__607 alunos evadindo__]. Porém, no ano anterior, 2023, com :blue[__1690 matrículas ativas__] contra :red[__622 desistências__].
+    st.warning('''2. __Evasão diminuindo, porém ainda sob atenção:__
+    - Em 2024, o nível de adesão alcançou em :blue[__1.054 matrículas ativas__], contra :red[__607 alunos evadindo__]. Porém, no ano anterior, 2023, com :blue[__690 matrículas ativas__] contra :red[__622 desistências__].
     - Com isso, observa-se embora o montante de alunos evadindo tenha aumentado em 2023, tivemos em 2024 uma melhora nesse quadro, representando um possível retorno de alunos que evadiram no ano anterior.
              ''')
