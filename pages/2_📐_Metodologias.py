a68c4f4c6101ceb860861feb02a0c782c2a2f994
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2025-02-14 01:04:40 -0300

    Update 2_📐_Metodologias.py

--- pages/2_📐_Metodologias.py
+++ pages/2_📐_Metodologias.py
@@ -1,20 +1,8 @@
 import streamlit as st
 import streamlit.components.v1 as components
-import plotly as plt
-import plotly.express as px
-import plotly.graph_objects as go
 import requests
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import streamlit as st
-import time
-import datetime
-from datetime import date, timedelta
-#from prophet import Prophet
-#from prophet.diagnostics import cross_validation, performance_metrics
-#from sklearn.metrics import mean_absolute_error, mean_squared_error
-from math import sqrt
 from streamlit_plotly_events import plotly_events
 from IPython.display import display
 
@@ -37,82 +25,102 @@ st.header('Metodologia')
 
 ## Visualização no Streamlit
 # Abas ('Tabs')
-aba1, aba2, aba3 = st.tabs(['Metodologia Machine Learning', 'Cenários Macroeconômicos', 'Plano para deploy'])
+aba1, aba2 = st.tabs(['Metodologia', 'Plano para deploy'])
 # aba1
 
-##REVISAR#######################################
 with aba1:
     st.write('''#### Metodologia do modelo de Machine Learning''')
     coluna1, coluna2 = st.columns(2)
     with coluna1:
         st.info('''__1. Coleta de Dados__''')
-        st.write('''- :blue[__Fonte dos Dados__]: Os dados foram extraídos da API da *U.S. Energy Information Administration (EIA)*, que fornece informações detalhadas sobre preços de petróleo, incluindo o *Brent*.
-- :blue[__Configuração da Requisiçãos__]: Foi usada a biblioteca *requests* para enviar solicitações HTTP. Os parâmetros da requisição incluem:
-    - __Produto:__ EPCBRENT (preço diário do *Brent*).
-    - __Data de início e fim:__ *start_date* e *end_date*.
-    - __Frequência:__ diária.
-    - __Paginação:__ A coleta foi implementada para lidar com paginação, permitindo extrair grandes volumes de dados, caso os resultados excedam o limite por requisição.
-    - __Estrutura dos Dados Recebidos:__ A API retorna os dados em formato JSON, contendo um campo chamado *value*, que corresponde ao preço do *Brent* em cada dia útil.''')
+        st.write('''- :blue[__Fonte dos Dados__]: Os dados foram extraídos do arquivo __PEDE 2024 - Dataton__, encontrado no diretório do Google Drive, devidamente referenciado na seção de *"Referências bibliográficas - Base de dados: PEDE 2024 (arquivo bruto), antes de ser tratado no BigQuery"*.''')
         st.info(''' __2. Limpeza e Preparação dos Dados__''')
-        st.write('''- :blue[__Transformação Inicial:__]
-    - Os dados coletados foram armazenados em um *DataFrame* do pandas.
-    - Colunas específicas foram renomeadas para o formato exigido pelo *Prophet*:
-        - ds (datas).
-        - y (valores a serem previstos).''')
-        st.write('''- :blue[__Tratamento de Dados Ausentes:__] Foi implementado o preenchimento ou remoção de valores ausentes, garantindo que a série temporal não tenha lacunas que comprometam o modelo.')''')
-        st.info(''' __3. Modelagem__''')
-        st.write('''- :blue[__Inicialização do Modelo *Prophet*:__] O modelo *Prophet* foi configurado e ajustado aos dados de treino. Alguns parâmetros podem ter sido ajustados:
-    - Sazonalidade:
-        - Adição de sazonalidade padrão (diária, semanal ou anual), que o *Prophet* detecta automaticamente.
-        - Configuração manual de sazonalidades específicas, se necessário.
-    - Hiperparâmetros:
-        - Intervalo de confiança padrão foi usado ou ajustado para maior precisão.''')
+        st.write('''- :blue[__Tratamento dos dados__]: Utilizou-se o *BigQuery com SQL* para programar a :blue[limpeza e tratamento] dos dados.''')
+        st.write('''- :blue[__Seleção das features__]: 
+    - __id_aluno (String):__ idade do aluno;
+    - __genero_aluno (String):__ gênero do aluno; 
+    - __ano_nasc (Integer):__ ano de nascimento do aluno;
+    - __ano_ingresso (Integer):__ ano que o aluno ingresso na ONG Passos Mágicos;
+    - __idade_2024 (Integer):__ idade do aluno em 2024;
+    - __turma_2024 (String):__ número da turma em 2024;
+    - __turma_2023 (String):__ número da turma em 2023;
+    - __turma_2022 (String):__ número da turma em 2022;
+    - __tipo_instituicao_2024 (String):__ tipo de instituição em 2024, e.g. pública ou particular;
+    - __tipo_instituicao_2023 (String):__ tipo de instituição em 2023, e.g. pública ou particular;
+    - __tipo_instituicao_2022 (String):__ tipo de instituição em 2022, e.g. pública ou particular;
+    - __fase_2024 (String):__ fase em 2024 do nível de aprendizado do aluno; 
+    - __fase_2023 (String):__ fase em 2023 do nível de aprendizado do aluno;
+    - __fase_2022 (String):__ fase em 2022 do nível de aprendizado do aluno;
+    - __pedra_2024 (String):__ Classificação do aluno em 2024 baseado no nº INDE, sendo categorizado em:
+        - Quartzo: de 2,405 a 5,506;
+        - Ágata: de 5,506 a 6,868;
+        - Ametista: de 6,868 a 8,230;
+        - Topázio: de 8,230 a 9,294.
+    - __pedra_2023 (String):__ Classificação do aluno em 2023 baseado no nº INDE, sendo categorizado em:
+        - Quartzo: de 2,405 a 5,506;
+        - Ágata: de 5,506 a 6,868;
+        - Ametista: de 6,868 a 8,230;
+        - Topázio: de 8,230 a 9,294.
+    - __pedra_2022 (String):__ Classificação do aluno em 2022 baseado no nº INDE, sendo categorizado em:
+        - Quartzo: de 2,405 a 5,506;
+        - Ágata: de 5,506 a 6,868;
+        - Ametista: de 6,868 a 8,230;
+        - Topázio: de 8,230 a 9,294.
+    - __pedra_2021 (String):__ Classificação do aluno em 2021 baseado no nº INDE, sendo categorizado em:
+        - Quartzo: de 2,405 a 5,506;
+        - Ágata: de 5,506 a 6,868;
+        - Ametista: de 6,868 a 8,230;
+        - Topázio: de 8,230 a 9,294.
+    - __pedra_2020 (String):__ Classificação do aluno em 2020 baseado no nº INDE, sendo categorizado em:
+        - Quartzo: de 2,405 a 5,506;
+        - Ágata: de 5,506 a 6,868;
+        - Ametista: de 6,868 a 8,230;
+        - Topázio: de 8,230 a 9,294.
+    - __inde_2024 (Float):__ índice do Desenvolvimento Educacional, como a ponderação dos indicadores IAN, IDA, IEG, IAA, IPS, IPP e IPV em 2024;
+    - __inde_2023 (Float):__ índice do Desenvolvimento Educacional, como a ponderação dos indicadores IAN, IDA, IEG, IAA, IPS, IPP e IPV em 2023;
+    - __inde_2022 (Float):__ índice do Desenvolvimento Educacional, como a ponderação dos indicadores IAN, IDA, IEG, IAA, IPS, IPP e IPV em 2022;
+
+''')
  
     with coluna2:
-        st.info(''' __4. Validação e Avaliação__''')
-        st.write('''- :blue[__Divisão dos Dados:__] Foi utilizada a validação cruzada com o módulo *cross_validation* do *Prophet*:
-    - A série temporal foi dividida em intervalos de __treino__ e __teste__.
-    - __Previsões__ foram realizadas para o conjunto de __teste__.''')
-        st.write('''- :blue[__Métricas de Desempenho:__] As previsões foram avaliadas com métricas como:
-    - MAE (Erro Médio Absoluto): mede o erro absoluto médio.
-    - RMSE (Erro Quadrático Médio): avalia a magnitude do erro.
-    - RAIZ(RMSE): fornece um valor interpretável para o erro médio em unidades reais.''')
-        st.write('''- :blue[__Comparação com Modelos Alternativos:__] O modelo foi comparado com outros métodos de previsão, como média ou tendência linear, para verificar sua robustez.''')
-        st.info(''' __5. Geração de Previsões__''')
-        st.write('''- :blue[__Horizonte de Previsão:__] As previsões foram geradas para um horizonte futuro especificado pelo usuário (número de dias).
-- :blue[__Intervalos de Confiança:__] Para cada previsão, o *Prophet* gerou intervalos de confiança que indicam a incerteza dos valores previstos.''')
-
-
+        st.write('''- :blue[__Seleção das features (continuação)__]: 
+    - __vlr_iaa_2024 (Float):__ indicador de Auto Avaliça em 2024;
+    - __vlr_iaa_2023 (Float):__ indicador de Auto Avaliça em 2023;
+    - __vlr_iaa_2022 (Float):__ indicador de Auto Avaliça em 2022;
+    - __vlr_ips_2024 (Float):__ indicador Psicossocial em 2024;
+    - __vlr_ips_2023 (Float):__ indicador Psicossocial em 2023;
+    - __vlr_ips_2022 (Float):__ indicador Psicossocial em 2022;
+    - __vlr_ipp_2024 (Float):__ indicador Psicopedagógico em 2024;
+    - __vlr_ipp_2023 (Float):__ indicador Psicopedagógico em 2023;
+    - __vlr_ipp_2022 (Integer):__ indicador Psicopedagógico em 2022;
+    - __vlr_ieg_2024 (Float):__ indicador de Engajamento em 2024;
+    - __vlr_ieg_2023 (Float):__ indicador de Engajamento em 2023;
+    - __vlr_ieg_2022 (Float):__ indicador de Engajamento em 2022;
+    - __vlr_ida_2024 (Float):__ indicador de Aprendizagem em 2024;
+    - __vlr_ida_2023 (Float):__ indicador de Aprendizagem em 2023;
+    - __vlr_ida_2022 (Float):__ indicador de Aprendizagem em 2022;
+    - __vlr_ipv_2024 (Float):__ indicador de ponto de virada em 2024;
+    - __vlr_ipv_2023 (Float):__ indicador de ponto de virada em 2023;
+    - __vlr_ipv_2022 (Float):__ indicador de ponto de virada em 2022;
+    - __vlr_ian_2024 (Float):__ indicador de adequação ao nível em 2024;
+    - __vlr_ian_2023 (Float):__ indicador de adequação ao nível em 2023;
+    - __vlr_ian_2022 (Float):__ indicador de adequação ao nível em 2022;
+    - __fase_ideal_2024 (String):__ fase ideal do aluno em 2024;
+    - __fase_ideal_2023 (String):__ fase ideal do aluno em 2023;
+    - __fase_ideal_2022 (String):__ fase ideal do aluno em 2022;
+    - __defasagem_2024 (Integer):__ nível de defasagem em 2024;
+    - __defasagem_2023 (Integer):__ nível de defasagem em 2023;
+    - __defasagem_2022 (Integer):__ nível de defasagem em 2022;
+    - __nome_instituicao_2024 (String):__ nome da instituição do aluno em 2024;
+    - __status_aluno_2024 (String):__ status do aluno em 2024, e.g. se o aluno está ativo ou se saiu da escola;
+''')
+        st.info(''' __3. Dashboard__''')
+        st.write('''- :blue[__Integração da base de dados:__] Foi utilizado o :blue[Looker] para consumo dos dados tratados no BigQuery e assim elaboração do Dashboard e criação dos indicadores.''')
+        st.write('''- :blue[__Elaboração do Dashboard:__] Com o :blue[Looker] enfocou-se na criação de avaliaões por Tipos de instituições de ensino de cada aluno, média do desempenho acadêmico dos alunos e avaliação das notas por perfis de aluno.''')
 
 
 
 with aba2:
-    st.write('''#### Cenário Macroeconômico e Preço do Petróleo (1987 - 2024)''')
-    coluna1, coluna2 = st.columns(2)
-    with coluna1:
-        st.info(''':blue[__Década de 1980-1990__]: Estabilidade e Primeiro Choque''')
-        st.write('''- Após a crise dos anos 1970, a década de :blue[1980 viu preços mais estáveis], com o barril de petróleo oscilando entre 15-25 USD;
-- Em 1990, a :blue[Guerra do Golfo elevou os preços], mas a rápida resolução do conflito estabilizou novamente o mercado.''')
-        st.info(''' :blue[__Década de 1990__]: Baixa Volatilidade''')
-        st.write('''- A década foi marcada por relativa :blue[estabilidade econômica global], com o barril permanecendo em torno de 20-30 USD, com oscilações moderadas;
-- A :blue[crise financeira asiática em 1997] reduziu temporariamente a demanda de petróleo, mas sem maiores impactos.''')
-        st.info(''' :blue[__2000-2010__]: Alta Volatilidade e Crises''')
-        st.write('''- A :blue[ascensão da China] e de outras economias emergentes aumentou a demanda por petróleo, :blue[elevando os preços];
-- :blue[Em 2008], o preço do barril chegou a 140 USD, antes de despencar para menos de 40 USD com a :blue[crise financeira global];
-- A :blue[invasão do Iraque em 2003] também gerou :blue[picos de preços].''')
-    with coluna2:
-        st.info(''' :blue[__2020-2024__]: Pandemia, Recuperação e Geopolítica''')
-        st.write('''- A pandemia de :blue[COVID-19 em 2020] gerou uma :blue[queda drástica na demanda] e os preços chegaram a ficar negativos por questões logísticas.
-- A :blue[recuperação econômica pós-pandemia impulsionou os preços] novamente para mais de 80 USD por barril.
-- A :blue[invasão da Ucrânia pela Rússia em 2022 elevou o preço] do petróleo, devido a sanções econômicas e interrupções nas cadeias de suprimento de energia.
-- A :blue[transição energética global] e o foco em energias renováveis começam a mudar a perspectiva de longo prazo da demanda por petróleo.''')
-        st.info('''  :blue[__Tendências Futuras__]''')
-        st.write('''- __Transição energética:__ A busca por alternativas renováveis deve :blue[reduzir demanda] global por petróleo gradualmente.
-- __Geopolítica e ESG:__ :blue[Conflitos] em regiões produtoras :blue[e políticas ambientais] de grandes economias continuarão a influenciar o mercado.
-- __Influência da China e da Índia:__ As economias emergentes, particularmente :blue[China e Índia], continuarão sendo grandes :blue[impulsionadores da demanda] de curto e médio prazo.''')
-
-
-with aba3:
     st.write('''#### Passo-a-passo para deploy''')
     coluna1, coluna2 = st.columns(2)
     with coluna1:
