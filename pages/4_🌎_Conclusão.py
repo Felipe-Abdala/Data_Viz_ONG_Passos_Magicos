132c1dd54f6f839ed33a0805be49b11c73dcfa27
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2025-02-14 21:02:43 -0300

    Update 4_🌎_Conclusão.py

--- pages/4_🌎_Conclusão.py
+++ pages/4_🌎_Conclusão.py
@@ -25,15 +25,19 @@ with coluna1:
     st.write('''1. :green[__Aumento no conjunto do desempenho acadêmico, psicossocial e psicopedagógico (INDE):__]
     - Entre os anos de 2022 e 2024, o resultado global do __*INDE*__, envolvendo as __áreas acadêmicas, psicossociais e psicopedagógicas__ totalizaram melhoras nos alunos do programa da ONG Passos Mágicos, tanto em :blue[escolas públicas evoluindo de 7,2 para 7,3], quanto para :blue[particulares migrando de 7,7 para 7,9].
              ''')
+    st.write('''''')
     st.write('''2. :green[__Aumento no conjunto do desempenho acadêmico, psicossocial e psicopedagógico (INDE):__]
     - O programa tem atendido efetivamente uma demanda intrínseca aos seus valores, o de ter impacto social, conforme visto na proporção de :blue[__alunos de escola pública__] versus __de escola particulares__, sendo o primeiro grupo o mais representativo.
              ''')
+    st.write('''''')
 with coluna2:
     st.warning('######  A partir da avaliação dos pontos a melhorar do programa, conclui-se que:', icon="⚠️")
     st.write('''1. :orange[__Dificuldade no desempenho acadêmico:__]
     - Durante os anos avaliados, identificou-se que o desempenho escolar (representado pelo "Indicador de Desempenho escolar", sigla IDA) apresentou queda quando comparados os anos de 2022 com o de 2024, tanto na média das :red[escolas públicas, indo de 6,45 para 6,22], quanto em :red[escolas particulares, indo de 7,1 para 7,0].
              ''')
+    st.write('''''')
     st.write('''2. :orange[__Evasão diminuindo, porém ainda sob atenção:__]
     - Em 2024, o nível de adesão alcançou em :blue[1.054 matrículas ativas], contra :red[607 alunos evadindo]. Porém, no ano anterior, 2023, com :blue[690 matrículas ativas] contra :red[622 desistências].
     - Com isso, observa-se embora o montante de alunos evadindo tenha aumentado em 2023, tivemos em 2024 uma melhora nesse quadro, representando um possível retorno de alunos que evadiram no ano anterior.
              ''')
+    st.write('''''')
