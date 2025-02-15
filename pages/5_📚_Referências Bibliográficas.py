02683b2dc17f34528f9b6b877d76312adb8b2dd0
Felipe Abdala <97188578+Felipe-Abdala@users.noreply.github.com>
2025-02-14 21:31:38 -0300

    Update 5_📚_Referências Bibliográficas.py

--- pages/5_📚_Referências Bibliográficas.py
+++ pages/5_📚_Referências Bibliográficas.py
@@ -21,13 +21,18 @@ st.info('''__Bibliotecas e Ferramentas__''')
 st.write('''- BigQuery: Base de dados tratada |  Disponível em: https://console.cloud.google.com/bigquery?ws=!1m5!1m4!4m3!1stech-challenge-fase-5!2s03_consumer_pede!3scz_inde_completo | Data de criação: 09/02/2025
 - Looker: Dashboard | Disponível em:  | Data de criação: 11/02/2025
 - GitHub: repositório com o Dashboard e configurações Streamlit | Disponível em: https://github.com/Felipe-Abdala/PEDE_ONG_Passos_Magicos | Data de criação: 11/02/2025''')
+st.write('''''')
 
 st.info('''__Fonte de Dados: Dicionários de dados, base de dados, relatórios anuais__''')
 st.write('''- Diretório com dicionário e bases de dados: Disponível em: https://drive.google.com/drive/folders/1Z1j6uzzCOgjB2a6i3Ym1pmJRsasfm7cD | Data de acesso: 05/02/2025
 - Base de dados: PEDE 2024 (arquivo bruto), antes de ser tratado no BigQuery | https://docs.google.com/spreadsheets/d/1td91KoeSgXrUrCVOUkLmONG9Go3LVcXpcNEw_XrL2R0/edit?gid=90992733#gid=90992733 | Data de acesso: 05/02/2025''')
+st.write('''''')
+
 st.info('''__Fonte de imagens__''')
 st.write('''-  Disponível em: https://passosmagicos.org.br/wp-content/uploads/2020/10/Passos-magicos-icon-cor.png | Data de acesso: 10/02/2025
 - Disponível em: https://niverdobem.com.br/wp-content/uploads/2020/10/18A3D82B-2FDB-4355-A9D1-FBAA99E56F41.jpeg | Data de acesso: 10/02/2025
 - Disponível em: https://passosmagicos.com.br/wp-content/uploads/2021/04/Design-sem-nome-5.png | Data de acesso: 10/02/2025''')
+st.write('''''')
+
 st.info('''__Conceitos de Storytelling e boas práticas em uso de gráficos e apresentações__''')
 st.write('''- Disponível em: https://www.storytellingwithdata.com/chart-guide | Data de acesso: 10/02/2025''')
